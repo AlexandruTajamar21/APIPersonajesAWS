9f7ef18575a75253fb58f9b67c026560c67e2938
Alexandru Preutesei <alexandru.preutesei@tajamar365.com>
2022-04-27 11:00:12 +0200

    Add project files.

---
+++ APIPersonajesAWS/Controllers/PersonajesController.cs
@@ -0,0 +1,42 @@
+﻿using APIPersonajesAWS.Models;
+using APIPersonajesAWS.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIPersonajesAWS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonajesController : ControllerBase
+    {
+        private RepositoryPersonajes repo;
+
+        public PersonajesController(RepositoryPersonajes repo)
+        {
+            this.repo = repo;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Personaje>> GetPersonajes()
+        {
+            return this.repo.GetPersonajes();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Personaje> FindPersonaje(int id)
+        {
+            return this.repo.FindPersonaje(id);
+        }
+
+        [HttpPost]
+        public ActionResult InsertarPersonaje(Personaje personaje)
+        {
+            this.repo.InsertPersonaje(personaje.IdPersonaje, personaje.Nombre, personaje.Imagen);
+            return Ok();
+        }
+    }
+}
---
+++ APIPersonajesAWS/Startup.cs
@@ -0,0 +1,68 @@
+using APIPersonajesAWS.Data;
+using APIPersonajesAWS.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIPersonajesAWS
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string url = this.Configuration.GetConnectionString("cadenaawsmysql");
+            services.AddTransient<RepositoryPersonajes>();
+            services.AddDbContext<PersonajesContext>(
+                options => options.UseMySql(url,ServerVersion.AutoDetect(url)));
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "APIPersonajesAWS", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "APIPersonajesAWS v1");
+                c.RoutePrefix = "";
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
